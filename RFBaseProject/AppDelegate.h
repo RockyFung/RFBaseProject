e75817e361d0284be8abbaa678d16ab29cfe7789
冯剑 <fengjian0213@126.com>
2019-03-19 13:59:05 +0800

    Initial Commit

---
+++ RFBaseProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RFBaseProject
+//
+//  Created by 冯剑 on 2019/3/19.
+//  Copyright © 2019 rocky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
